a876cea1abe0b5532a2badc38428b309636d769e
adrbnss <adrienbns.pro@gmail.com>
2023-09-25 17:43:37 +0200

    more fixing

--- contracts/Covid.sol
+++ contracts/Covid.sol
@@ -7,254 +7,6 @@ Discord:
 
 */
 
-// File: @openzeppelin/contracts/utils/cryptography/MerkleProof.sol
-
-// OpenZeppelin Contracts (last updated v4.8.0) (utils/cryptography/MerkleProof.sol)
-
-pragma solidity ^0.8.0;
-
-/**
- * @dev These functions deal with verification of Merkle Tree proofs.
- *
- * The tree and the proofs can be generated using our
- * https://github.com/OpenZeppelin/merkle-tree[JavaScript library].
- * You will find a quickstart guide in the readme.
- *
- * WARNING: You should avoid using leaf values that are 64 bytes long prior to
- * hashing, or use a hash function other than keccak256 for hashing leaves.
- * This is because the concatenation of a sorted pair of internal nodes in
- * the merkle tree could be reinterpreted as a leaf value.
- * OpenZeppelin's JavaScript library generates merkle trees that are safe
- * against this attack out of the box.
- */
-library MerkleProof {
-    /**
-     * @dev Returns true if a `leaf` can be proved to be a part of a Merkle tree
-     * defined by `root`. For this, a `proof` must be provided, containing
-     * sibling hashes on the branch from the leaf to the root of the tree. Each
-     * pair of leaves and each pair of pre-images are assumed to be sorted.
-     */
-    function verify(
-        bytes32[] memory proof,
-        bytes32 root,
-        bytes32 leaf
-    ) internal pure returns (bool) {
-        return processProof(proof, leaf) == root;
-    }
-
-    /**
-     * @dev Calldata version of {verify}
-     *
-     * _Available since v4.7._
-     */
-    function verifyCalldata(
-        bytes32[] calldata proof,
-        bytes32 root,
-        bytes32 leaf
-    ) internal pure returns (bool) {
-        return processProofCalldata(proof, leaf) == root;
-    }
-
-    /**
-     * @dev Returns the rebuilt hash obtained by traversing a Merkle tree up
-     * from `leaf` using `proof`. A `proof` is valid if and only if the rebuilt
-     * hash matches the root of the tree. When processing the proof, the pairs
-     * of leafs & pre-images are assumed to be sorted.
-     *
-     * _Available since v4.4._
-     */
-    function processProof(
-        bytes32[] memory proof,
-        bytes32 leaf
-    ) internal pure returns (bytes32) {
-        bytes32 computedHash = leaf;
-        for (uint256 i = 0; i < proof.length; i++) {
-            computedHash = _hashPair(computedHash, proof[i]);
-        }
-        return computedHash;
-    }
-
-    /**
-     * @dev Calldata version of {processProof}
-     *
-     * _Available since v4.7._
-     */
-    function processProofCalldata(
-        bytes32[] calldata proof,
-        bytes32 leaf
-    ) internal pure returns (bytes32) {
-        bytes32 computedHash = leaf;
-        for (uint256 i = 0; i < proof.length; i++) {
-            computedHash = _hashPair(computedHash, proof[i]);
-        }
-        return computedHash;
-    }
-
-    /**
-     * @dev Returns true if the `leaves` can be simultaneously proven to be a part of a merkle tree defined by
-     * `root`, according to `proof` and `proofFlags` as described in {processMultiProof}.
-     *
-     * CAUTION: Not all merkle trees admit multiproofs. See {processMultiProof} for details.
-     *
-     * _Available since v4.7._
-     */
-    function multiProofVerify(
-        bytes32[] memory proof,
-        bool[] memory proofFlags,
-        bytes32 root,
-        bytes32[] memory leaves
-    ) internal pure returns (bool) {
-        return processMultiProof(proof, proofFlags, leaves) == root;
-    }
-
-    /**
-     * @dev Calldata version of {multiProofVerify}
-     *
-     * CAUTION: Not all merkle trees admit multiproofs. See {processMultiProof} for details.
-     *
-     * _Available since v4.7._
-     */
-    function multiProofVerifyCalldata(
-        bytes32[] calldata proof,
-        bool[] calldata proofFlags,
-        bytes32 root,
-        bytes32[] memory leaves
-    ) internal pure returns (bool) {
-        return processMultiProofCalldata(proof, proofFlags, leaves) == root;
-    }
-
-    /**
-     * @dev Returns the root of a tree reconstructed from `leaves` and sibling nodes in `proof`. The reconstruction
-     * proceeds by incrementally reconstructing all inner nodes by combining a leaf/inner node with either another
-     * leaf/inner node or a proof sibling node, depending on whether each `proofFlags` item is true or false
-     * respectively.
-     *
-     * CAUTION: Not all merkle trees admit multiproofs. To use multiproofs, it is sufficient to ensure that: 1) the tree
-     * is complete (but not necessarily perfect), 2) the leaves to be proven are in the opposite order they are in the
-     * tree (i.e., as seen from right to left starting at the deepest layer and continuing at the next layer).
-     *
-     * _Available since v4.7._
-     */
-    function processMultiProof(
-        bytes32[] memory proof,
-        bool[] memory proofFlags,
-        bytes32[] memory leaves
-    ) internal pure returns (bytes32 merkleRoot) {
-        // This function rebuild the root hash by traversing the tree up from the leaves. The root is rebuilt by
-        // consuming and producing values on a queue. The queue starts with the `leaves` array, then goes onto the
-        // `hashes` array. At the end of the process, the last hash in the `hashes` array should contain the root of
-        // the merkle tree.
-        uint256 leavesLen = leaves.length;
-        uint256 totalHashes = proofFlags.length;
-
-        // Check proof validity.
-        require(
-            leavesLen + proof.length - 1 == totalHashes,
-            "MerkleProof: invalid multiproof"
-        );
-
-        // The xxxPos values are "pointers" to the next value to consume in each array. All accesses are done using
-        // `xxx[xxxPos++]`, which return the current value and increment the pointer, thus mimicking a queue's "pop".
-        bytes32[] memory hashes = new bytes32[](totalHashes);
-        uint256 leafPos = 0;
-        uint256 hashPos = 0;
-        uint256 proofPos = 0;
-        // At each step, we compute the next hash using two values:
-        // - a value from the "main queue". If not all leaves have been consumed, we get the next leaf, otherwise we
-        //   get the next hash.
-        // - depending on the flag, either another value for the "main queue" (merging branches) or an element from the
-        //   `proof` array.
-        for (uint256 i = 0; i < totalHashes; i++) {
-            bytes32 a = leafPos < leavesLen
-                ? leaves[leafPos++]
-                : hashes[hashPos++];
-            bytes32 b = proofFlags[i]
-                ? leafPos < leavesLen ? leaves[leafPos++] : hashes[hashPos++]
-                : proof[proofPos++];
-            hashes[i] = _hashPair(a, b);
-        }
-
-        if (totalHashes > 0) {
-            return hashes[totalHashes - 1];
-        } else if (leavesLen > 0) {
-            return leaves[0];
-        } else {
-            return proof[0];
-        }
-    }
-
-    /**
-     * @dev Calldata version of {processMultiProof}.
-     *
-     * CAUTION: Not all merkle trees admit multiproofs. See {processMultiProof} for details.
-     *
-     * _Available since v4.7._
-     */
-    function processMultiProofCalldata(
-        bytes32[] calldata proof,
-        bool[] calldata proofFlags,
-        bytes32[] memory leaves
-    ) internal pure returns (bytes32 merkleRoot) {
-        // This function rebuild the root hash by traversing the tree up from the leaves. The root is rebuilt by
-        // consuming and producing values on a queue. The queue starts with the `leaves` array, then goes onto the
-        // `hashes` array. At the end of the process, the last hash in the `hashes` array should contain the root of
-        // the merkle tree.
-        uint256 leavesLen = leaves.length;
-        uint256 totalHashes = proofFlags.length;
-
-        // Check proof validity.
-        require(
-            leavesLen + proof.length - 1 == totalHashes,
-            "MerkleProof: invalid multiproof"
-        );
-
-        // The xxxPos values are "pointers" to the next value to consume in each array. All accesses are done using
-        // `xxx[xxxPos++]`, which return the current value and increment the pointer, thus mimicking a queue's "pop".
-        bytes32[] memory hashes = new bytes32[](totalHashes);
-        uint256 leafPos = 0;
-        uint256 hashPos = 0;
-        uint256 proofPos = 0;
-        // At each step, we compute the next hash using two values:
-        // - a value from the "main queue". If not all leaves have been consumed, we get the next leaf, otherwise we
-        //   get the next hash.
-        // - depending on the flag, either another value for the "main queue" (merging branches) or an element from the
-        //   `proof` array.
-        for (uint256 i = 0; i < totalHashes; i++) {
-            bytes32 a = leafPos < leavesLen
-                ? leaves[leafPos++]
-                : hashes[hashPos++];
-            bytes32 b = proofFlags[i]
-                ? leafPos < leavesLen ? leaves[leafPos++] : hashes[hashPos++]
-                : proof[proofPos++];
-            hashes[i] = _hashPair(a, b);
-        }
-
-        if (totalHashes > 0) {
-            return hashes[totalHashes - 1];
-        } else if (leavesLen > 0) {
-            return leaves[0];
-        } else {
-            return proof[0];
-        }
-    }
-
-    function _hashPair(bytes32 a, bytes32 b) private pure returns (bytes32) {
-        return a < b ? _efficientHash(a, b) : _efficientHash(b, a);
-    }
-
-    function _efficientHash(
-        bytes32 a,
-        bytes32 b
-    ) private pure returns (bytes32 value) {
-        /// @solidity memory-safe-assembly
-        assembly {
-            mstore(0x00, a)
-            mstore(0x20, b)
-            value := keccak256(0x00, 0x40)
-        }
-    }
-}
-
 interface IUniswapV2Factory {
     function createPair(
         address tokenA,
@@ -624,7 +376,7 @@ contract SARSCOV2 is
 
     bool public isTradingEnabled = false;
 
-    bool public swapEnabled = true;
+    bool public swapEnabled = false;
     uint256 public swapThreshold = (_totalSupply / 1000) * 3; // 0.3%
 
     bool inSwap;
@@ -655,14 +407,15 @@ contract SARSCOV2 is
         mapping(uint256 => address) requestIdToAddy;
         mapping(address => uint256) addyToRequestId;
         mapping(address => bool) requestFullfilled; // getter
-        address[10] gotLuckyVaccine; // getter
+        address[] gotLuckyVaccine; // getter
     }
 
     // Store users vaccines through epochs
     mapping(address => uint256) public userCurrentVaccine;
 
     // Store list of users who bought a vaccine in any epoch
-    address[] public userVaccineList;
+    mapping(address => bool) public userHasBoughtVaccine;
+    address[] internal userVaccineList;
 
     // Epoch id
     uint256 public epochId = 0;
@@ -711,6 +464,8 @@ contract SARSCOV2 is
     uint256 internal immutable vaccineTwoReductionRateSell = 914;
     uint256 internal immutable vaccineThreeReductionRateSell = 1157; // Tier 3 vaccine aka the most effective
 
+    // Handle game start and end
+    bool public isGameStarted = false;
     bool public isGameOver = false;
 
     uint256 currentPendingRewards;
@@ -742,6 +497,8 @@ contract SARSCOV2 is
 
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _setupRole(ADMIN_ROLE, msg.sender);
+
+        epochs[epochId].capsulePrice = 10 ether;
 
         rewardPool = new RewardPool(address(this));
 
@@ -800,7 +557,9 @@ contract SARSCOV2 is
 
     function _startNewEpoch() internal {
         // choix de conserver son ancien vaccin ou de le remettre en jeu (tjrs 1 fois par epoch)
-        rewardPool.distributeShares(epochs[epochId].gotLuckyVaccine);
+        if (epochs[epochId].gotLuckyVaccine.length > 0) {
+            rewardPool.distributeShares(epochs[epochId].gotLuckyVaccine);
+        }
 
         epochId++;
         epochs[epochId].id = epochId;
@@ -820,8 +579,12 @@ contract SARSCOV2 is
         // Auto downgrade
         for (uint256 i = 0; i < userVaccineList.length; i++) {
             if (userCurrentVaccine[userVaccineList[i]] > 0) {
-                userCurrentVaccine[userVaccineList[i]]--;
+                userCurrentVaccine[userVaccineList[i]] -= 1;
             }
+        }
+
+        if (!isGameStarted) {
+            isGameStarted = true;
         }
 
         _upgradeVaccineProtection();
@@ -883,7 +646,7 @@ contract SARSCOV2 is
         address recipient,
         uint256 amount
     ) external override returns (bool) {
-        return _transferFrom(msg.sender, recipient, amount);
+        return _basicTransfer(msg.sender, recipient, amount);
     }
 
     function setMaxWallet(uint256 _maxWalletSize_) external onlyOwner {
@@ -970,7 +733,6 @@ contract SARSCOV2 is
         amountOfInfection[msg.sender]++;
         amountOfAllInfection++;
 
-        _balances[msg.sender] = _balances[msg.sender] - 1 ether;
         bool temp;
 
         temp = _basicTransfer(msg.sender, _toInfect, 1 ether);
@@ -980,6 +742,7 @@ contract SARSCOV2 is
         uint256 _capsulePrice = epochs[epochId].capsulePrice;
 
         require(infected[msg.sender], "You are not infected");
+        require(isGameStarted, "Game has not started");
         require(
             balanceOf(msg.sender) >= _capsulePrice,
             "Not enough tokens to buy a capsule"
@@ -1000,8 +763,6 @@ contract SARSCOV2 is
         capsuleCount++;
         capsuleCurrentCount++;
         epochs[epochId].isBuyer[msg.sender] = true;
-
-        _balances[msg.sender] = _balances[msg.sender] - _capsulePrice;
 
         uint256 _amountToBurn = (_capsulePrice * 200) / 1000; // 20% burn
 
@@ -1025,8 +786,8 @@ contract SARSCOV2 is
     function openCapsule(address user) external {
         require(user == msg.sender, "Not authorized");
         require(
-            epochs[epochId].endTime < block.timestamp,
-            "Epoch is not over, wait for the next one"
+            epochs[epochId].endTime > block.timestamp,
+            "Epoch is over, wait for the next one"
         );
         require(
             epochs[epochId].addyToRequestId[msg.sender] != 0,
@@ -1064,8 +825,9 @@ contract SARSCOV2 is
     }
 
     function getVaccine(address user, uint256 vaccine, bool vaccine4) internal {
-        if (userCurrentVaccine[user] == 0) {
+        if (!userHasBoughtVaccine[user]) {
             userVaccineList.push(user);
+            userHasBoughtVaccine[user] = true;
         }
         userCurrentVaccine[user] = vaccine;
         epochs[epochId].hasOpenedCapsule[user] = true;
@@ -1099,7 +861,6 @@ contract SARSCOV2 is
         );
 
         epochs[epochId].hasUpgrade[msg.sender] = true;
-        _balances[msg.sender] = _balances[msg.sender] - _capsulePrice;
 
         uint256 _amountForDev = (_capsulePrice * 50) / 1000; // 5% for dev
 
@@ -1172,7 +933,6 @@ contract SARSCOV2 is
             amountOfInfection[msg.sender]++;
             amountOfAllInfection++;
 
-            _balances[msg.sender] = _balances[msg.sender] - 1 ether;
             bool temp;
             temp = _basicTransfer(msg.sender, _toInfect[i], 1 ether);
         }
@@ -1256,6 +1016,7 @@ contract SARSCOV2 is
         address recipient,
         uint256 amount
     ) internal returns (bool) {
+        _balances[sender] = _balances[sender] - amount;
         _balances[recipient] = _balances[recipient] + (amount);
         emit Transfer(sender, recipient, amount);
         return true;
@@ -1348,8 +1109,6 @@ contract SARSCOV2 is
 
         uint256 _pendingRewards = pendingRewards[msg.sender];
         pendingRewards[msg.sender] = 0;
-
-        _balances[address(this)] = _balances[address(this)] - _pendingRewards;
 
         if (block.timestamp < launchTime + 1 days) {
             _pendingRewards = (_pendingRewards * 70) / 100;
